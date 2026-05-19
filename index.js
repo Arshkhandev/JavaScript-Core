caedcf15d84f6d690c21ea80765c329c88eaaa0a
Arshkhandev <arsh.khan.dev@gmail.com>
2026-05-19 21:59:48 +0530

    "two pointer logic i study in java but now  can apply in javascript as well

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hii");
+// function Palidrome(s){
+//    let left =0;
+//    let right =s.length-1;
+//    while(left<right){
+//     if(s.charAt(left) != s.charAt(right)){
+//         return false;
+//     }
+//     left++;
+//     right--;
+//    }
+//    return true;
+// }
+
+
+//  let str = "racecar"
+// console.log(Palidrome(str)); 
