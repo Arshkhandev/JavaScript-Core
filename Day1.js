4ff8481a4669bb024904cb99a5aead46c1deaf8d
Arshkhandev <arsh.khan.dev@gmail.com>
2026-05-21 12:08:48 +0530

    come javascript basic rivision

---
+++ Day1.js
@@ -0,0 +1,21 @@
+// let age =21;
+// // let age=30;
+// // const a =10;
+// // a=30;
+// var name = "arshkhan";
+// var name="arsh";
+
+
+// console.log(age);
+// console.log(name);
+
+
+let a=10;
+let b =20;
+let temp=b
+b=a;
+a=temp;
+;
+console.log(a);
+console.log(b);
+console.log(temp);
