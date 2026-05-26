a66b670146209c51c2120928b4080bb02427eef2
Arshkhandev <arsh.khan.dev@gmail.com>
2026-05-26 21:25:47 +0530

    type basics

--- Day5.js
+++ Day5.js
@@ -5,3 +5,16 @@
 //     return fac * factorial(fac-1);
 // }
 // console.log(factorial(3));
+
+
+
+function factorial(fac){
+    if(fac == 0 || fac ==1){
+        return 1;
+    }
+    return fac * factorial(fac-1);
+}
+console.log(factorial(3));
+
+
+console.log("hello ji")
