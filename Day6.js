353d9d58a5d0e4bf725efcade30b7d72e501a7ac
Arshkhandev <arsh.khan.dev@gmail.com>
2026-05-29 09:26:50 +0530

    some basics of javascript

--- Day6.js
+++ Day6.js
@@ -154,3 +154,61 @@ function RemoveDuplicate(nums){
 console.log(RemoveDuplicate([1,2,2,3,3,4,4,4,5]));
 
 
+
+
+
+
+
+function CountVowel(str){
+    let count=0;
+      for(let i=0;i<str.length;i++){
+          if(str[i].toLowerCase() === 'a' || str[i].toLowerCase() === 'e' || str[i].toLowerCase() ===  'i' || str[i].toLowerCase() === 'o' || str[i].toLowerCase() === 'u'){
+            count++;
+          }
+      }
+       return count;
+}
+
+
+console.log(CountVowel("str"));
+
+
+function RemoveDuplicate(nums){
+   let result = [];
+   for(let i=0;i<nums.length;i++){
+    if(!result.includes(nums[i])){
+        result.push(nums[i]);
+    }
+   }
+  return result;
+}
+
+console.log(RemoveDuplicate([1,2,2,3,3,4,4,4,5]));
+
+
+function CountVowel(str){
+    let count=0;
+    for(let ch of str.toLowerCase() ){
+        if("aeiou".includes(ch)){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(CountVowel("str"));
+
+
+function RemoveDuplicate(nums){
+   let result = [];
+   for(let i=0;i<nums.length;i++){
+    if(!result.includes(nums[i])){
+        result.push(nums[i]);
+    }
+   }
+  return result;
+}
+
+console.log(RemoveDuplicate([1,2,2,3,3,4,4,4,5]));
+
+
