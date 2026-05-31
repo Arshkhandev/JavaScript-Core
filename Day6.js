59307ff569588e2be55cd1cc566a02f9c986f4b9
Arshkhandev <arsh.khan.dev@gmail.com>
2026-05-31 14:44:03 +0530

    basics only rev

--- Day6.js
+++ Day6.js
@@ -215,3 +215,7 @@ console.log(RemoveDuplicate([1,2,2,3,3,4,4,4,5]));
 
 
 console.log("helllo jeee");
+console.log("helllo jeee");
+console.log("helllo jeee");
+console.log("helllo jeee");
+console.log("helllo jeee");
