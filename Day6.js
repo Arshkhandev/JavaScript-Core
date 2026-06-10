82f6666ea408d27e2c49f626cba3a30964f29f2c
Arshkhandev <arsh.khan.dev@gmail.com>
2026-06-10 17:14:27 +0530

    day 7

--- Day6.js
+++ Day6.js
@@ -146,10 +146,6 @@ function RemoveDuplicate(nums){
 
 console.log(RemoveDuplicate([1,2,2,3,3,4,4,4,5]));
 
-console.log("hellojiii");
 
-console.log("hellojiii");
-
-console.log("hellojiii");
 
 console.log("hellojiii");
